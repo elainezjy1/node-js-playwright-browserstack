576cb590600217f1d088c4d2d09358ff66f2ac9d
Elaine Zhang <junying.zhang@anz.com>
2024-11-07 19:43:33 +1100

    Remove unused code

--- deloitte_tests/support/hooks.js
+++ deloitte_tests/support/hooks.js
@@ -1,8 +1,6 @@
-import { LaunchOptions, Logger, test as base } from '@playwright/test';
-// import { test } from './myfixture';
+import { test as base } from '@playwright/test';
 const StorePage = require('../pages/storePage');
 const CartPage = require('../pages/cartPage');
-const LogSeverity = 'verbose' | 'info' | 'warning' | 'error';
 
 base.beforeAll('Set up', async () => {
     console.log('Custom hooks: Before tests');
@@ -16,7 +14,7 @@ base.beforeEach(async ({ page }) => {
 base.afterEach(async ({ page }) => {
     console.log(`Finished ${test.info().title} with status ${test.info().status}`);
     if (test.info().status !== test.info().expectedStatus) {
-        await page.screenshot({ path: `./deloitte_tests/screenshots/${test.info().title}.png`, fullPage: true })
+        await page.screenshot({ path: `./screenshots/${test.info().title}.png`, fullPage: true })
     }
 })
 
@@ -36,13 +34,6 @@ const test = base.extend({
         const storePage = new StorePage(page);
         await use(storePage);
         console.log('Custom fixture: teardown storePage');
-    },
-    logger: async ({ }, use) => {
-        const logger = {
-            isEnabled: (name, LogSeverity) => name === 'TEST_LOG',
-            log: (name, LogSeverity, message, args) => console.log(`${name} ${severity} ${message} ${args.join(' ')}`)
-        }
-        await use({ logger });
     }
 });
 
